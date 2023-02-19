3c947aa31833cdb758409f09f47b2a353594cff6
Drac28 <daswieselvomjura@gmail.com>
2023-02-19 23:41:39 +0100

    v2.0
    Now generating videos instead of Photos. Even some optimizations are already in place

--- __main__.py
+++ __main__.py
@@ -1,48 +1,49 @@
 import numpy as np
 import cv2
 import copy
-from tqdm import tqdm
 import os
 import standard
+import json
+from tqdm import tqdm
+from multiprocessing import Process
 
-def count_frames(path):
-    (major, minor, _) = cv2.__version__.split(".")
-    video = cv2.VideoCapture(path)
-    total = 0
+def count_frames(origin, path):
     try:
-        if major == "3":
-            total = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        elif major == "2":
-            total = int(video.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+        if os.path.exists(origin+"/.cache"):
+            total = json.load(open(origin+"/.cache"))[path]
         else:
             raise Exception
     except:
-        while True:
-            try:
-                (grabbed, frame) = video.read()
-                if not grabbed:
+        (major, minor, _) = cv2.__version__.split(".")
+        video = cv2.VideoCapture(origin+"/"+path)
+        total = 0
+        try:
+            if major == "3":
+                total = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+            elif major == "2":
+                total = int(video.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+            else:
+                raise Exception
+        except:
+            while True:
+                try:
+                    (grabbed, frame) = video.read()
+                    if not grabbed:
+                        break
+                    total += 1
+                except KeyboardInterrupt:
                     break
-                total += 1
-            except KeyboardInterrupt:
-                break
     print(total, "frames in total")
     return total
 
-def main(path, outpath, divider, level, frames=0):
-    cap = cv2.VideoCapture(path)
+def main(origin, path, num, divider, level, frames=0):
+    outpath = path+"_"+str(num)+".mp4"
+    cap = cv2.VideoCapture(origin+"/"+path)
     fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
-
-    if frames == 0:
-        all_frames = count_frames(path)
-        frames = int(all_frames / divider)
-    else:
-        all_frames = frames
-        frames = int(frames / divider)
-
-    print(frames, "frames to process")
+    currentframe = 0
 
     first_iteration_indicator = 1
-    for i in tqdm(range(0, frames)):
+    for i in tqdm(range(0, frames), position=num, desc="Frame generation"):
         '''
         There are some important reasons this if statement exists:
             -in the first run there is no previous frame, so this accounts for that
@@ -71,15 +72,19 @@ def main(path, outpath, divider, level, frames=0):
             maxValue = int(2 * divider / level)
             ret, th1 = cv2.threshold(fgmask, thresh, maxValue, cv2.THRESH_BINARY)
             # for testing purposes, show the threshold image
-            # cv2.imwrite('diff-th1.jpg', th1)
+            # cv2.imshow('diff-th1.jpg', th1)
 
             # add to the accumulated image
             accum_image = cv2.add(accum_image, th1)
             # for testing purposes, show the accumulated image
-            # cv2.imwrite('diff-accum.jpg', accum_image)
+            # cv2.imshow('diff-accum.jpg', accum_image)
 
             # for testing purposes, control frame by frame
-            # raw_input("press any key to continue")
+            color_image = im_color = cv2.applyColorMap(accum_image, cv2.COLORMAP_HOT)
+            result_overlay = cv2.addWeighted(frame, 0.7, color_image, 0.7, 0)
+            if not os.path.exists(origin+"/working/"+outpath):
+                os.mkdir(origin+"/working/"+outpath)
+            cv2.imwrite(origin+"/working/"+outpath+"/"+str(currentframe)+".jpg", result_overlay)
 
         # for testing purposes, show the current frame
         # cv2.imshow('frame', gray)
@@ -89,19 +94,23 @@ def main(path, outpath, divider, level, frames=0):
                 cap.read()
             except:
                 pass
+        currentframe += 1
 
-    # apply a color map
-    # COLORMAP_PINK also works well, COLORMAP_BONE is acceptable if the background is dark
-    color_image = im_color = cv2.applyColorMap(accum_image, cv2.COLORMAP_HOT)
-    # for testing purposes, show the colorMap image
-    # cv2.imwrite('diff-color.jpg', color_image)
+    home = os.getcwd()
+    standard.rename(origin+"/working/"+outpath)
+    os.chdir(home)
+
+    out = cv2.VideoWriter(origin+"/output/"+outpath, cv2.VideoWriter_fourcc(*'mp4v'), 15, (width, height))
+    working_dir = standard.sort_nicely(os.listdir(origin+"/working/"+outpath))
+    for i in tqdm(range(0, len(working_dir)), position=num, desc="Video generation"):
+        img = cv2.imread(origin+"/working/"+outpath+"/"+working_dir[i])
+        out.write(img)
+    out.release()
+
+    standard.remove(origin+"/working/"+outpath)
 
     # overlay the color mapped image to the first frame
     result_overlay = cv2.addWeighted(first_frame, 0.7, color_image, 0.7, 0)
-
-    # save the final overlay image
-    print(outpath)
-    cv2.imwrite(outpath, result_overlay)
 
     # cleanup
     cap.release()
@@ -114,18 +123,41 @@ if __name__=='__main__':
         divider = standard.input_int("All Frames divided by: ")
         frames = {}
         if os.path.isdir(path):
-            for file in os.listdir(path):
+            standard.mkdir(path)
+            for file in tqdm(os.listdir(path), desc="Files"):
                 if not os.path.isdir(path+"/"+file):
-                    print("preview:", file)
-                    frames[file] = main(path+"/"+file, path+"/output/"+file+"_preview.jpg", divider*4, 1)
+                    if file != ".cache":
+                        print("counting:", file)
+                        frames[file] = count_frames(path, file)
             print(frames)
-            for file in os.listdir(path):
+            json.dump(frames, open(path+"/.cache", "w"), sort_keys=True, indent=4)
+            for file in tqdm(frames, position=0, desc="General"):
                 if not os.path.isdir(path+"/"+file):
                     print(file)
-                    main(path+"/"+file, path+"/output/"+file+"_1.jpg", divider, 1, frames[file])
-                    main(path+"/"+file, path+"/output/"+file+"_2.jpg", divider, 1.5, frames[file])
-                    main(path+"/"+file, path+"/output/"+file+"_3.jpg", divider, 2, frames[file])
+                    p1 = Process(target=main, args=(path, file, 1, divider, 1, frames[file], ))
+                    p2 = Process(target=main, args=(path, file, 2, divider, 1.5, frames[file]))
+                    p3 = Process(target=main, args=(path, file, 3, divider, 2, frames[file]))
+                    p1.start()
+                    p2.start()
+                    p3.start()
+                    p1.join()
+                    p2.join()
+                    p3.join()
         else:
-            main(path)
+            path, file = os.path.split(path)
+            standard.mkdir(path)
+            print("preview:", file)
+            frames[file] = main(path, path+"/"+file, file+"_preview.mp4", divider*4, 1)
+            print(frames)
+            print(file)
+            p1 = Process(target=main, args=(path, path+"/"+file, file+"_1.mp4", divider, 1, frames[file], ))
+            p2 = Process(target=main, args=(path, path+"/"+file, file+"_2.mp4", divider, 1.5, frames[file]))
+            p3 = Process(target=main, args=(path, path+"/"+file, file+"_3.mp4", divider, 2, frames[file]))
+            p1.start()
+            p2.start()
+            p3.start()
+            p1.join()
+            p2.join()
+            p3.join()
     except KeyboardInterrupt:
         print("User sent exit signal.\nBye")
--- standard.py
+++ standard.py
@@ -1,3 +1,6 @@
+import os
+import re
+
 def input_int(text):
     while True:
         try:
@@ -8,3 +11,94 @@ def input_int(text):
         except:
             pass
     return in_int
+
+def mkdir(path):
+    if not os.path.exists(path+"/output"):
+        os.mkdir(path+"/output")
+    if not os.path.exists(path+"/working"):
+        os.mkdir(path+"/working")
+    
+
+def name_len(file):
+    basename = os.path.splitext(os.path.basename(file))[0]
+    name = ""
+    for char in basename:
+        try:
+            int(char)
+            name += char
+        except:
+            print(name)
+            return len(name)
+    return len(name)
+
+def fill_to_max(file, max_len):
+    if name_len(file) == 0:
+        return file
+    while name_len(file) < max_len:
+        file = "0"+file
+    return file
+
+def rename_folder(folder):
+    files = os.listdir()
+    if len(files) < 10:
+        max_len = 1
+    elif len(files) < 100:
+        max_len = 2
+    elif len(files) < 1000:
+        max_len = 3
+    else:
+        max_len = 0
+
+    for file in files:
+        new_name = fill_to_max(file, max_len)
+        os.rename(file, new_name)
+
+def folder_start(folder, basefolder):
+    value = os.listdir()
+    files = []
+    folders = []
+    for file in value:
+        if os.path.isdir(folder+"/"+file):
+            folders.append(file)
+        else:
+            files.append(file)
+
+    rename_folder(folder)
+
+    for folder in folders:
+        os.chdir(folder)
+        rename_folder(folder)
+        os.chdir("..")
+
+def rename(folder):
+    os.chdir(folder)
+    folder_start(folder, folder)
+
+def tryint(s):
+    try:
+        return int(s)
+    except:
+        return s
+
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [ tryint(c) for c in re.split('([0-9]+)', s) ]
+
+def sort_nicely(l):
+    """ Sort the given list in the way that humans expect.
+    """
+    l.sort(key=alphanum_key)
+    return l
+
+def remove(path):
+    files = os.listdir(path)
+    for file in files:
+        try:
+            for file1 in os.listdir(path+"/"+file):
+                os.remove(path+"/"+file+"/"+file1)
+            os.rmdir(path+"/"+file)
+        except:
+            os.remove(path+"/"+file)
+    os.rmdir(path)
